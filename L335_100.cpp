8c2c063bb0c8374492b953cd9f19893a7a072c76
P4nzerfaust <a.a.volkov98@mail.ru>
2025-10-13 21:39:30 +0500

    Волков Александр МХ-201

---
+++ L335_100.cpp
@@ -0,0 +1,19 @@
+﻿void deleteNode(ListNode*& head, ListNode* nodeToDelete) {
+    if (!head || !nodeToDelete) return;
+
+    // Проверяем принадлежность узла списку
+    if (!isNodeInList(head, nodeToDelete)) return;
+
+    if (head == nodeToDelete) {
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+    else {
+        ListNode* prev = findPrevious(head, nodeToDelete);
+        if (prev && prev->next == nodeToDelete) {
+            prev->next = nodeToDelete->next;
+            delete nodeToDelete;
+        }
+    }
+}
