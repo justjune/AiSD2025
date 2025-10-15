26f5be7e39b97c709d8802d3f83f7e7abbec8e77
P4nzerfaust <a.a.volkov98@mail.ru>
2025-10-15 22:41:54 +0500

    L336_110

---
+++ L336_110.cpp
@@ -0,0 +1,12 @@
+﻿template<typename T>
+struct TreeNode {
+    T item;                    // Элемент данных
+    TreeNode<T>* parent;       // Указатель на родительский узел
+    TreeNode<T>* left;         // Указатель на левый дочерний узел
+    TreeNode<T>* right;        // Указатель на правый дочерний узел
+
+    // Конструктор
+    TreeNode(const T& value, TreeNode<T>* p = nullptr)
+        : item(value), parent(p), left(nullptr), right(nullptr) {
+    }
+};
