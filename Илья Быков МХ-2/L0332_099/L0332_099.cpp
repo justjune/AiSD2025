9e026ecd842869da8a084dcd2d24b8a1c118da2c
ba1kov <bykov31ilya@gmail.com>
2025-10-21 00:03:07 +0500

    Create L0332_099.cpp

---
+++ Илья Быков МХ-2/L0332_099/L0332_099.cpp
@@ -0,0 +1,29 @@
+//L0332_099
+//рекурсивный поиск элемента в связном списке
+//Быков Илья МХ-2 20.10.25
+#include <iostream>
+
+// Предполагаем, что структура List определена следующим образом:
+template <typename T>
+struct List {
+    T item;
+    List<T>* next;
+    List(T val) : item(val), next(nullptr) {}
+};
+
+template <typename T>
+List<T>* search_list(List<T>* l, T x) {
+    // Если список пуст, возвращаем nullptr
+    if (l == nullptr) {
+        return nullptr;
+    }
+
+    // Если элемент текущего узла равен искомому значению
+    if (l->item == x) {
+        // Возвращаем указатель на текущий узел
+        return l;
+    } else {
+        // Иначе, рекурсивно вызываем функцию поиска для следующего узла списка
+        return search_list(l->next, x);
+    }
+}
