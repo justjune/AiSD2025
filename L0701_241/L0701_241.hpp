2bdf8813a3bd504c01af8a89128f69a6fd9b5147
kopicentr <vitalijkarass@gmail.com>
2025-10-29 01:23:39 +0500

    L0701_241

---
+++ L0701_241/L0701_241.hpp
@@ -0,0 +1,6 @@
+#ifndef L0701_241_HPP
+#define L0701_241_HPP
+struct List;
+List* search_list(List* l, int x);
+List* append(List* head, int value);
+#endif
---
+++ L0701_241/L0701_241_test.cpp
@@ -0,0 +1,14 @@
+//Карасс Виталий, МХ-201, Список смежности
+//L0701_241_test
+
+#include "L0701_241.hpp"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main() {
+Graph graph;				// Создаём граф
+return 0;
+}
