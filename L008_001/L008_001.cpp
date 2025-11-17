690151df78fdaa337b58b627a3b939afb2525da5
kit-sunne <ro.ngl@yandex.ru>
2025-11-17 23:40:17 +0500

    L008_001

--- L008_001/L008_001.cpp
+++ L008_001/L008_001.cpp
@@ -1,2 +1,14 @@
-#include<iostream>
-using namespace std
+struct graph{
+    edgenode *edges[MAXV+1];  /* Массив указателей на списки смежности */
+    int degree[MAXV+1];       /* Степень каждой вершины */
+    int nvertices;            /* Количество вершин в графе */
+    int nedges;               /* Количество ребер в графе */
+    int directed;             /* Граф ориентированный? */
+};
+
+
+typedef struct edgenode {
+    int y;                    /* Номер смежной вершины */
+    int weight;               /* Вес ребра, если есть */
+    struct edgenode *next;    /* Указатель на следующее ребро в списке */
+} edgenode;
