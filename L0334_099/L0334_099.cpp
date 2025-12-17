3206c3fa9283f7be7a910640bc8eefbf0e474ffb
ba1kov <bykov31ilya@gmail.com>
2025-12-17 21:24:30 +0500

    Update L0334_099.cpp

--- L0334_099/L0334_099.cpp
+++ L0334_099/L0334_099.cpp
@@ -17,25 +17,4 @@ List* item_ahead(List* l, List* x) {
     return item_ahead(l->next, x);
 }
 
-int main() {
-    List* node1 = new List(1);
-    List* node2 = new List(2);
-    List* node3 = new List(3);
-
-    node1->next = node2; // Создаем связь node1 -> node2
-    node2->next = node3; // Создаем связь node2 -> node3
-
-    List* result = item_ahead(node1, node2); // Ищем, идет ли node2 за node1
-    if (result != nullptr) {
-        std::cout << "Success!" << std::endl;
-    } else {
-        std::cout << "Failure!" << std::endl;
-    }
-
-    //Освободим память
-    delete node1;
-    delete node2;
-    delete node3;
-    return 0;
-}
 #endif
