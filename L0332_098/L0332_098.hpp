822d1529ab70acb9201cea62524e39a7fed14f92
ba1kov <bykov31ilya@gmail.com>
2025-12-17 21:00:51 +0500

    Create L0332_098.hpp

---
+++ L0332_098/L0332_098.hpp
@@ -0,0 +1,10 @@
+#ifndef L0332_098_HPP
+#define L0332_098_HPP
+#include <iostream>
+template <typename T> // Предполагаем, что структура List определена следующим образом:
+struct List {
+    T item; // Определение типа узла
+    List<T>* next; // Создание указателя на следующий узел
+    List(T val) : item(val), next(nullptr) {} //Конструктор создания нового узла
+};
+#endif
