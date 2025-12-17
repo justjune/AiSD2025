bbce5c74c4ed1da16bd3abee7d5637064f2b495d
ba1kov <bykov31ilya@gmail.com>
2025-12-17 21:45:05 +0500

    Update L0334_099test.cpp

--- L0334_099/L0334_099test.cpp
+++ L0334_099/L0334_099test.cpp
@@ -10,13 +10,14 @@ int main() {
     List* empty_list = nullptr;
     List* node1 = new List(1);
     assert(item_ahead(empty_list, node1) == nullptr);
-    
+    //Важно убрать утечку памяти:
     delete node1;
 
     // Тест 2: Один элемент в списке
     node1 = new List(1);
     List* node2 = new List(2);
     assert(item_ahead(node1, node2) == nullptr);
+    //Важно убрать утечки памяти:
     delete node1;
     delete node2;
 
@@ -25,6 +26,7 @@ int main() {
     node2 = new List(2);
     node1->next = node2;
     assert(item_ahead(node1, node2) == node1);
+    //Важно убрать утечку памяти:
     delete node1;
 
     return 0;
