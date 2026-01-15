4620de6e7befe585aca209e3a5dd97bf44c9dc2d
xeutep-meqpa <unrealvlad56@gmail.com>
2026-01-15 22:51:36 +0400

    Add files via upload

---
+++ L0434_152/L434_152.cpp
@@ -0,0 +1,23 @@
+﻿#include "PriorityQueue.hpp"
+#include <iostream>
+#include <vector>
+void demo_pyramid()
+{
+    PriorityQueue<int> pq;
+    PriorityQueue<int> fast_test;
+    vector<int> vec = { 4, 5, 7, 5, 2, 1 };
+    fast_test.make_heap_fast(vec);
+    fast_test.print();
+
+    int i = pq.heap_compare(3, 6, 12);
+    cout << "\nheap compare k = " << i << '\n';
+    vector<int> numbers = { 3, 1, 4, 1, 5, 9, 2, 6 };
+    auto sorted_numbers = heap_sort(numbers);
+    cout << '\n';
+    for (auto s : sorted_numbers)
+        cout << s << ' ';
+}
+int main(){
+    demo_pyramid();
+    return 0;
+}
