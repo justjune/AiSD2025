09f1d6810f1d5d377ad2ea44f7c21f24e8bdb005
artemcheburin <artem.cheburin2005@gmail.com>
2026-01-15 13:28:40 +0500

    Delete L309_112 directory

--- L309_112/L039_112.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "tree.h"
-void traverse_tree(tree* l);
--- L309_112/L039_112test.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include <stddef.h>
-#include "L039_112.h"
-
-int main()
-{
-    tree* l = NULL;
-    traverse_tree(l);
-}
