72b25e1b96796d732a2cf1a907bd4d51a8511abf
Marrryyrrr <maria.ivanova26@mail.ru>
2026-01-15 15:04:33 +0500

    L0908_332

---
+++ L0908_332/L0908_332.hpp
@@ -0,0 +1,11 @@
+#ifndef L0908_332_HPP
+#define L0908_332_HPP
+
+#include "L0907_331.hpp"
+
+#define NMAX 100  // Максимальное количество вершин
+
+// Процедура для построения кандидатов следующей вершины в пути
+void construct_candidates(int a[], int k, const ath_data* data, int c[], int* nc);
+
+#endif
